b5019651f81fb5e4ccfeef0bf490924229d38c15
haodengtengfa <956450236@qq.com>
2016-10-25 10:15:46 +0800

    统计图

---
+++ src/com/ht/bean/CustomerGc.java
@@ -0,0 +1,27 @@
+package com.ht.bean;
+
+/**
+ * �ͻ����ɷ���ʵ��
+ * @author Administrator
+ *
+ */
+public class CustomerGc {
+
+	private String customerLevel; // �ͻ��ȼ�
+	private int customerNum; // �ͻ�����
+	
+	public String getCustomerLevel() {
+		return customerLevel;
+	}
+	public void setCustomerLevel(String customerLevel) {
+		this.customerLevel = customerLevel;
+	}
+	public int getCustomerNum() {
+		return customerNum;
+	}
+	public void setCustomerNum(int customerNum) {
+		this.customerNum = customerNum;
+	}
+	
+	
+}
